d870309efe18581234f0830241b93a8802919032
Bangun Herwanto <herwanto.bangun@gmail.com>
2024-04-09 11:51:11 +0700

    Implementasi Atomic Design Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
 import React from "react";
-
-class Button extends React.Component {
-  render() {
-    return (
-      <button
-        className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
-        type="submit"
-      >
-        Buy Now
-      </button>
-    );
-  }
-}
-
-function ButtonBlack() {
-  return (
-    <button
-      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
-      type="submit"
-    >
-      Buy Now
-    </button>
-  );
-}
-
-const ButtonRed = () => {
-  return (
-    <button
-      className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white"
-      type="submit"
-    >
-      Buy Now
-    </button>
-  );
-};
+import Button from "./components/Elements/Buttons";
 
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-2">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
         <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
-        <ButtonBlack></ButtonBlack>
-        <ButtonRed></ButtonRed>
       </div>
     </div>
   );
