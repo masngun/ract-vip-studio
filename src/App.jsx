aa39c3c6eee71a863de71897eba39a4fb0457761
Bangun Herwanto <herwanto.bangun@gmail.com>
2024-05-25 06:23:24 +0700

    contoh membuat form tanpa dipecah

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,42 @@ import Button from "./components/Elements/Buttons";
 
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-2">
-        <Button variant="bg-red-700">Login</Button>
-        <Button variant="bg-slate-700">Logout</Button>
-        <Button></Button>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, Please enter your details
+        </p>
+        <form action="">
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block text-slate-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="example@mail.com"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-slate-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="****"
+            />
+          </div>
+          <Button classname="bg-blue-600 w-full">Login</Button>
+        </form>
       </div>
     </div>
   );
